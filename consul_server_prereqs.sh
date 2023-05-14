5c26f2cbdb05d39174d4e7aab9953e808bbf2c3f
jharshman <joshgreyhat@gmail.com>
2023-05-13 19:53:05 -0600

    functioning install, tls protection, and acl.

--- consul_server_prereqs.sh
+++ consul_server_prereqs.sh
@@ -31,21 +31,31 @@ main() {
     esac
   done
 
+  if [[ -z $_domain ]]; then
+    usage
+    exit 1
+  fi
+
   # create ca
-  $CONSUL_BIN tls ca create -name-constraint -domain="$_domain"
+  $CONSUL_BIN tls ca create \
+    -domain="$_domain"      \
+    ;
 
   # create server certificates
   $CONSUL_BIN tls cert create -server \
-    -ca="$_domain-agent-ca.pem" \
-    -key="$_domain-agent-ca-key.pem" \
-    -domain="$_domain"
+    -ca="$_domain-agent-ca.pem"       \
+    -key="$_domain-agent-ca-key.pem"  \
+    -domain="$_domain"                \
+    ;
 
-  # create gossip key
+  # create gossip key and initial management token
   # store in file and as var
   gossip_key=$($CONSUL_BIN keygen)
+  init_mgmt_token=$(cat /proc/sys/kernel/random/uuid)
   echo $gossip_key > gossip.key
-  cat << EOM > roles/server/vars/gossip.yml
+  cat << EOM > roles/server/vars/generated.yml
 consul_gossip_key: $gossip_key
+consul_init_mgmt_token: $init_mgmt_token
 EOM
 
   # move to roles/server/files/secrets/
@@ -59,17 +69,39 @@ EOM
   # encrypt with ansible-vault and randomly generated password
   randPass=$(pwgen 10 1)
   echo $randPass > password_file.txt
+  echo -n "encrypting tls certs and keys... "
   ansible-vault encrypt --vault-password-file password_file.txt roles/server/files/secrets/*
-  ansible-vault encrypt --vault-password-file password_file.txt roles/server/vars/gossip.yml
+  echo -n "encrypting gossip key... "
+  ansible-vault encrypt --vault-password-file password_file.txt roles/server/vars/generated.yml
 
   # ensure that "password_file" is in .gitignore
   if [[ ! -f .gitignore ]]; then
     # create a .gitignore
     echo "password_file.txt" > .gitignore
-  elif ! grep 'password_file.txt' .gitignore; then
+  elif ! grep -q 'password_file.txt' .gitignore; then
     # append to .gitignore
     echo "password_file.txt" >> .gitignore
   fi
+
+  cat <<EOM
+
+-----------------------------------------------------------
+Run the following command to install andn configure Consul.
+
+  ansible-playbook -e @roles/server/vars/generated.yml --vault-password-file password_file.txt -i all.ini playbook.yml
+
+-----------------------------------------------------------
+
+To access Consul through the CLI, run the following in your terminal session.
+
+  ansible-vault view --vault-password-file password_file.txt roles/server/files/secrets/ca.pem > ~/consul_ca.pem
+  export CONSUL_HTTP_ADDR=https://server.dc1.$_domain:8501
+  export CONSUL_HTTP_SSL=true
+  export CONSUL_CACERT=~/consul_ca.pem 
+  export CONSUL_TLS_SERVER_NAME=server.dc1.$_domain
+  export CONSUL_FQDN_ADDR=harshman.me
+  export CONSUL_HTTP_TOKEN=$init_mgmt_token
+EOM
 
 }
 
